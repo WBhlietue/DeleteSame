6859f0abc037a0303523eae54f7ca92e870dc705
WBhlietue <WBhlietue@gmail.com>
2022-10-10 16:26:05 +0800

    add win && change level

--- js/main.js
+++ js/main.js
@@ -7,8 +7,15 @@ var yNum = 7;
 var xOffset = 60;
 var yOffset = 80;
 
+var s = 0;
+var h = 0;
+var m = 0;
+
 var main = document.getElementById("main");
 main.isPlay = true;
+
+var win = document.getElementById("win");
+win.style.scale = 0;
 
 var preSelected = null;
 
@@ -20,38 +27,81 @@ var randoms = [
   "#00ffff",
   "#2200ff",
   "#bb00ff",
-  "#ff7af4",
-  "#632727",
-  "#688781",
 ];
 
 var colors = [];
 var number = 0;
 
-for (var i = 0; i < xNum; i++) {
-  for (var j = 0; j < yNum; j++) {
-    var x = startX + xOffset * i;
-    var y = startY + yOffset * j;
-    var item = document.createElement("div");
-    item.className = "item";
-    item.style.left = x;
-    item.style.top = y;
-    number++;
-    if(number <= xNum * yNum/2){
-        var num = Math.floor(Math.random() * randoms.length);
-        colors.push(num);
-    }else{
-        k = Math.floor(Math.random() * colors.length)
-        var num = colors[k];
-        colors.splice(k, 1);
+document.getElementById("x").value = 2;
+document.getElementById("y").value = 2;
+
+function Start() {
+  xNum = document.getElementById("x").value;
+  yNum = document.getElementById("y").value;
+  if ((yNum * xNum) % 2 != 0) {
+    alert("x + y must divided by 2");
+  } else {
+    startX = (1200 - 30 * xNum - 30 * (xNum - 1)) / 2;
+    startY = (600 - 50 * yNum - 30 * (yNum - 1)) / 2;
+    document.getElementById("start").style.scale = 0;
+    document.getElementById("start").style.top = 150;
+    document.getElementById("start").style.left = 450;
+    for (var i = 0; i < xNum; i++) {
+      for (var j = 0; j < yNum; j++) {
+        var x = startX + xOffset * i;
+        var y = startY + yOffset * j;
+        var item = document.createElement("div");
+        item.className = "item";
+        item.style.left = x;
+        item.style.top = y;
+        number++;
+        if (number <= (xNum * yNum) / 2) {
+          var num = Math.floor(Math.random() * randoms.length);
+          colors.push(num);
+        } else {
+          k = Math.floor(Math.random() * colors.length);
+          var num = colors[k];
+          colors.splice(k, 1);
+        }
+        item.color = randoms[num];
+        item.num = num;
+        item.clicked = false;
+        item.setAttribute("onCLick", "OnClick(this)");
+        main.appendChild(item);
+      }
     }
-    item.color = randoms[num];
-    // item.style.backgroundColor = randoms[num];
-    item.num = num;
-    item.clicked = false;
-    item.setAttribute("onCLick", "OnClick(this)");
-    main.appendChild(item);
   }
+  StartTimer();
+}
+
+function StartTimer() {
+  var timer = document.getElementById("timer");
+  timer.innerHTML = "00:00:00";
+  const innnnn = setInterval(function () {
+    s++;
+    if (s >= 60) {
+      s = 0;
+      m++;
+    }
+    if (m >= 60) {
+      m = 0;
+      h++;
+    }
+    var value =
+      (h < 10 ? "0" : "") +
+      h +
+      ":" +
+      (m < 10 ? "0" : "") +
+      m +
+      ":" +
+      (s < 10 ? "0" : "") +
+      s;
+    timer.innerHTML = value;
+    if (!main.isPlay) {
+      timer.innerHTML = "";
+      clearInterval(innnnn);
+    }
+  }, 1000);
 }
 
 function OnClick(item) {
@@ -103,7 +153,40 @@ function DelItem(item) {
   item.style.left = parseInt(item.style.left) + 25;
   item.style.top = parseInt(item.style.top) + 25;
   number--;
-  if(number == 0){
-      main.isPlay = false;
+  if (number == 0) {
+    main.style.display = "none";
+    win.style.scale = 1;
+    console.log(win.style.scale);
+    main.isPlay = false;
+    var value =
+      (h < 10 ? "0" : "") +
+      h +
+      ":" +
+      (m < 10 ? "0" : "") +
+      m +
+      ":" +
+      (s < 10 ? "0" : "") +
+      s;
+    document.getElementById("winTime").innerHTML = "You spended " + value;
+  }
+}
+
+function ReStart() {
+  location.reload();
+}
+
+function InputOnChange(id) {
+  var v = document.getElementById(id);
+  if (id == "x") {
+    if (v.value > 18) {
+      v.value = 18;
+    }
+  } else {
+    if (v.value > 7) {
+      v.value = 7;
+    }
+  }
+  if (v.value < 2) {
+    v.value = 2;
   }
 }
